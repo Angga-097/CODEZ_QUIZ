c92429d1bfa82ab77c46e9bca112361bb4766103
Angga-097 <138885631+Angga-097@users.noreply.github.com>
2024-01-04 19:21:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,388 @@
+
+const questions = [
+    {
+        question: "Pemesanan tempat memory variable dan tipe data atau fungsi tertentu adalah… ?",
+        optionA: "Algoritma",
+        optionB: "Flowchart",
+        optionC: "Nilai data", 
+        optionD: "Deklarasi",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Urutan instruksi untuk menyelesaikan masalah yang disusun secara logis dan sistematis dengan menggunakan bahasa pemrograman adalah… ?",
+        optionA: "Input",
+        optionB: "Pemrograman",
+        optionC: " Struktur",
+        optionD: "Algoritma",
+        correctOption: "optionB"  
+    },
+
+    {
+        question: "Data yang harus diberikan pada komputer disebut… ?",
+        optionA: "Output",
+        optionB: "Deklarasi",
+        optionC: "Flowchart",
+        optionD: "Input",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Langkah-langkah untuk menyelesaikan masalah secara logis dan sistematis adalah… ?",
+        optionA: "Flowchart",
+        optionB: "Pemrograman",
+        optionC: "Algoritma",
+        optionD: "Nilai data",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Apa yang dimaksud dengan pseudocode ?",
+        optionA: "Bahasa melayu",
+        optionB: "Bahasa inggris",
+        optionC: "Bahasa komputer",
+        optionD: "Bahasa sehari-hari",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Setiap perintah dalam project C++ selalu diakhiri dengan tanda ?",
+        optionA: ";",
+        optionB: ">",
+        optionC: "{}",
+        optionD: "//",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Apakah fungsi dari tanda {} pada bahasa C++ ?",
+        optionA: "Sebagai tanda akhir perintah",
+        optionB: "Untuk memanggil fungsi main",
+        optionC: "Sebagai blok statement, menyatukan dua atau lebih statement menjadi satu statement",
+        optionD: "Memberikan komentar",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Bahasa pemrograman C++ dibuat oleh ?",
+        optionA: "Bjarne Stroudtrup",
+        optionB: "Rasmus Lerdorf",
+        optionC: "Niklaus Wirth",
+        optionD: "Mads Torgersen",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Tipe data dalam bentuk pecahan disebut ?",
+        optionA: "Int",
+        optionB: "Double",
+        optionC: "Char",
+        optionD: "Float",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Hubungan antara angka dan huruf disebut ?",
+        optionA: "Boolean",
+        optionB: "Float",
+        optionC: "String",
+        optionD: "Variabel",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Tipe data integer diartikan sebagai ?",
+        optionA: "Karakter",
+        optionB: "Bilangan pecahan",
+        optionC: "Bilangan bulat",
+        optionD: "Angka desimal",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Tipe data dalam bentuk karakter adalah ?",
+        optionA: "Char",
+        optionB: "Float",
+        optionC: "Int",
+        optionD: "Double",
+        correctOption: "optionA"
+    }, 
+
+    {
+        question: "Bagian dari program yang memiliki peran penting dan akan mempengaruhi setiap intruksi jalannya program adalah ?",
+        optionA: "Fungsi",
+        optionB: "Tipe data",
+        optionC: "Input",
+        optionD: "Array",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Cin dalam program C++ digunakan untuk ?",
+        optionA: "Mengolah data",
+        optionB: "Membuat program",
+        optionC: "Menghasilkan output",
+        optionD: "Menginput data",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Cout dalam program C++ berarti ?",
+        optionA: "Menampilkan output ke layar",
+        optionB: "Mengetahui adanya error",
+        optionC: "Mengolah data",
+        optionD: "Membuat program",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Menyatakan fungsi utama berakhir di running di jalankan tanpa error adalah fungsi dari ?",
+        optionA: "getch",
+        optionB: "cout",
+        optionC: "return0",
+        optionD: "conio.h",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Tipe data yang menyatakan TRUE FALSE dalam sebuah program adalah ?",
+        optionA: "Boolean",
+        optionB: "Integer",
+        optionC: "Character",
+        optionD: "String",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Variabel yang menyimpan nilai data untuk di proses didalam fungsi adalah ?",
+        optionA: "Fungsi",
+        optionB: "Deklarasi",
+        optionC: "Parameter",
+        optionD: "Void",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Struktur dasar algoritma pemrograman adalah ?",
+        optionA: "Sequence, selection, dan looping",
+        optionB: "Array, Pointer, dan Variabel",
+        optionC: "String, Char, dan Boolean",
+        optionD: "Deklarasi, Flowchart, dan Output",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Tipe data untuk menyimpan variabel berupa nilai teks adalah ?",
+        optionA: "Float",
+        optionB: "Integer",
+        optionC: "String",
+        optionD: "Char",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Suatu kondasi dimana struktur if berada di dalam if lainnya adalah statment dari kondisi ?",
+        optionA: "If bersarang",
+        optionB: "If while",
+        optionC: "If then",
+        optionD: "If else",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Tipe data terstruktur yang terdiri dari sejumlah komponen yang mempunyai tipe sama disebut ?",
+        optionA: "Integer",
+        optionB: "Byte",
+        optionC: "Array",
+        optionD: "Pointer",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Suatu program terpisah dalam blok sendiri yang berfungsi sebagai subprogram program bagian disebut ?",
+        optionA: "Array",
+        optionB: "Pointer",
+        optionC: "Variabel Global",
+        optionD: "Prosedur/Fungsi",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Tahap prodes distribusi output kepada pihak yang membutuhkan informasi disebut ?",
+        optionA: "Distribution",
+        optionB: "Processing",
+        optionC: "Origanation",
+        optionD: "Input",
+        correctOption: "optionA"
+    },
+
+    {
+        question: "Struktur dasar program yang dibuat dengan C++ terdiri dari tiga bagian diantaranya, kecuali ?",
+        optionA: "Namespace",
+        optionB: "Fungsi",
+        optionC: "Includ",
+        optionD: "Getch",
+        correctOption: "optionD"
+    },
+
+    {
+        question: "Jenis file yang akan diimpor .cpp berarti ?",
+        optionA: "Header file dari C++;",
+        optionB: "Source code dari C++;",
+        optionC: "Header file dari C;",
+        optionD: "Header file dari C",
+        correctOption: "optionB"
+    },
+
+    {
+        question: "Apa yang dimaksud dengan tipe data numerik ?",
+        optionA: "Urutan list dari beberapa nilai berbeda",
+        optionB: "Penyimpanan data berupa karakter",
+        optionC: "Penyimpanan data berupa angka",
+        optionD: "Tipe data true atau false",
+        correctOption: "optionC"
+    },
+
+    {
+        question: "Tipe data yang mempresentasikan True atau False disebut ?",
+        optionA: "Numerik",
+        optionB: "Enumerasi",
+        optionC: "Boolean",
+        optionD: "String",
+        correctOption: "optionC"
+    }
+];
+
+let shuffledQuestions = [];
+
+function handleQuestions() {
+    while (shuffledQuestions.length <= 9) {
+        const random = questions[Math.floor(Math.random() * questions.length)];
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random);
+        }
+    }
+}
+
+let questionNumber = 1;
+let playerScore = 0;
+let wrongAttempt = 0;
+let indexNumber = 0;
+
+function NextQuestion(index) {
+    handleQuestions();
+    const currentQuestion = shuffledQuestions[index];
+    document.getElementById('question-number').innerHTML = questionNumber;
+    document.getElementById('player-score').innerHTML = playerScore;
+    document.getElementById('display-question').innerHTML = currentQuestion.question;
+    document.getElementById('option-one-label').innerHTML = currentQuestion.optionA;
+    document.getElementById('option-two-label').innerHTML = currentQuestion.optionB;
+    document.getElementById('option-three-label').innerHTML = currentQuestion.optionC;
+    document.getElementById('option-four-label').innerHTML = currentQuestion.optionD;
+}
+
+function checkForAnswer() {
+    const currentQuestion = shuffledQuestions[indexNumber];
+    const currentQuestionAnswer = currentQuestion.correctOption;
+    const options = document.getElementsByName('option');
+    let correctOption = null;
+
+    options.forEach((option) => {
+        if (option.value === currentQuestionAnswer) {
+            correctOption = option.labels[0].id;
+        }
+    });
+
+    if (options[0].checked === false && options[1].checked === false && options[2].checked === false && options[3].checked === false) {
+        document.getElementById('option-modal').style.display = 'flex';
+    }
+
+    options.forEach((option) => {
+        if (option.checked === true && option.value === currentQuestionAnswer) {
+            document.getElementById(correctOption).style.backgroundColor = 'green';
+            playerScore++;
+            indexNumber++;
+
+            setTimeout(() => {
+                questionNumber++;
+            }, 1000);
+        } else if (option.checked && option.value !== currentQuestionAnswer) {
+            const wrongLabelId = option.labels[0].id;
+            document.getElementById(wrongLabelId).style.backgroundColor = 'red';
+            document.getElementById(correctOption).style.backgroundColor = 'green';
+            wrongAttempt++;
+            indexNumber++;
+
+            setTimeout(() => {
+                questionNumber++;
+            }, 1000);
+        }
+    });
+}
+
+function handleNextQuestion() {
+    checkForAnswer();
+    unCheckRadioButtons();
+
+    setTimeout(() => {
+        if (indexNumber <= 9) {
+            NextQuestion(indexNumber);
+        } else {
+            handleEndGame();
+        }
+        resetOptionBackground();
+    }, 1000);
+}
+
+function resetOptionBackground() {
+    const options = document.getElementsByName('option');
+    options.forEach((option) => {
+        document.getElementById(option.labels[0].id).style.backgroundColor = '';
+    });
+}
+
+function unCheckRadioButtons() {
+    const options = document.getElementsByName('option');
+    for (let i = 0; i < options.length; i++) {
+        options[i].checked = false;
+    }
+}
+
+function handleEndGame() {
+    let remark = null;
+    let remarkColor = null;
+
+    if (playerScore <= 3) {
+        remark = 'Bad Grades, Keep Practicing.';
+        remarkColor = 'red';
+    } else if (playerScore >= 4 && playerScore < 7) {
+        remark = 'Average Grades, You can do better.';
+        remarkColor = 'orange';
+    } else if (playerScore >= 7) {
+        remark = 'Excellent, keep the good work going.';
+        remarkColor = 'green';
+    }
+
+    const playerGrade = (playerScore / 10) * 100;
+
+    document.getElementById('remarks').innerHTML = remark;
+    document.getElementById('remarks').style.color = remarkColor;
+    document.getElementById('grade-percentage').innerHTML = playerGrade;
+    document.getElementById('wrong-answers').innerHTML = wrongAttempt;
+    document.getElementById('right-answers').innerHTML = playerScore;
+    document.getElementById('score-modal').style.display = 'flex';
+}
+
+function closeScoreModal() {
+    questionNumber = 1;
+    playerScore = 0;
+    wrongAttempt = 0;
+    indexNumber = 0;
+    shuffledQuestions = [];
+    NextQuestion(indexNumber);
+    document.getElementById('score-modal').style.display = 'none';
+}
+
+function closeOptionModal() {
+    document.getElementById('option-modal').style.display = 'none';
+}
